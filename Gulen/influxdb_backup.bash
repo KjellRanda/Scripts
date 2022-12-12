bd3e36812226b106b3f037c44f9118df61d7f6d7
pi <pi@git.net.home>
2022-12-12 20:48:58 +0100

    Influxdb docker backup script

---
+++ Gulen/influxdb_backup.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ECHO="/bin/echo"
+#
+CONTAINER="influxdb"
+#
+nc=`docker ps | grep -c $CONTAINER`
+if [ $nc -eq 1 ]
+then
+   ID=`docker ps | grep $CONTAINER | awk '{print $1}'`
+else
+   $ECHO ""
+   $ECHO "Container $CONTAINER not found"
+   $ECHO ""
+   exit 1
+fi
+#
+dirdate=$(date +'%m.%d.%Y')
+#
+docker exec -t $ID mkdir /opt/$dirdate
+docker exec -t $ID influxd backup --portable /opt/$dirdate
+#
