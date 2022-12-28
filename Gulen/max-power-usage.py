ed699255c27a19162dba3643f08e7c03a8d1c3c3
Kjell Randa <kjell.m.randa@gmail.com>
2022-12-28 23:49:01 +0100

    Some pylint fixes

--- Gulen/max-power-usage.py
+++ Gulen/max-power-usage.py
@@ -1,25 +1,30 @@
 
 from datetime import datetime
-from time import mktime
 import time
 import logging
 from influxdb import InfluxDBClient
 
 def datetime_from_utc_to_local(utc):
+    """
+    Convert date and time from UTC to local time
+    """
     epoch = time.mktime(time.strptime(utc, '%Y-%m-%dT%H:%M:%SZ'))
     offset = datetime.fromtimestamp(epoch) - datetime.utcfromtimestamp(epoch)
     return (datetime.strptime(utc, '%Y-%m-%dT%H:%M:%SZ') + offset).strftime('%Y-%m-%d %H:%M:%S')
 
-def influx_json(val, time, type):
+def influx_json(val, timev, typev):
+    """
+    Create json structure for line protocol in Influxdb
+    """
     json_body = [
         {
             "measurement": "mqtt_consumer",
             "tags": {
-                "Maxusage": type
+                "Maxusage": typev
             },
             "fields": {
                 "value": val,
-                "date": time
+                "date": timev
             }
         }
     ]
@@ -35,38 +40,38 @@ maxuse = ["MAX1", "MAX2", "MAX3"]
 
 year = datetime.now().year
 month = datetime.now().month
-sdate = str(year) + "-" + str(month) + "-01T00:00:01Z"
+SDATE = str(year) + "-" + str(month) + "-01T00:00:01Z"
 
 client = InfluxDBClient(host='localhost', port='8086', database='hansensor')
 
 SQL = "SELECT INTEGRAL(\"mean\")/3600 FROM ( SELECT MEAN(\"val\") AS mean FROM \"mqtt_consumer\" " + \
       "WHERE \"topic\" = \'pt:j1/mt:evt/rt:dev/rn:zigbee/ad:1/sv:meter_elec/ad:1_1\' AND \"unit\" = \'W\' AND time <= now() and time >= \'" + \
-       sdate + "\' GROUP BY time(5s) fill(previous) ) GROUP BY time(1h)"
+       SDATE + "\' GROUP BY time(5s) fill(previous) ) GROUP BY time(1h)"
 result = client.query(SQL)
 
-n = 0
+NUM = 0
 points = result.get_points()
 for point in points:
     arr.append([point['integral'],point['time']])
-    n = n + 1
+    NUM = NUM + 1
 
-if n <= 3:
+if NUM <= 3:
     logging.warning("Not enough data to contine. Only " + str(n) + " found")
     client.close()
-    exit()
+    exit(1)
 
 arr.sort(reverse=True)
 
-sum = 0.0
+sumu = 0.0
 for i in range(3):
     x, s = arr[i]
     pusage.append(x/1000)
     ustime.append(datetime_from_utc_to_local(s))
-    sum = sum + pusage[i]
+    sumu = sumu + pusage[i]
     jbody = influx_json(pusage[i], ustime[i], maxuse[i])
     client.write_points(jbody)
 
-avg = sum/3
+avg = sumu/3
 jbody = influx_json(avg, ustime[0], "MAXAVG")
 client.write_points(jbody)
 
