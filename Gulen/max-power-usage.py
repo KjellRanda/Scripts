f3aea8026224e8228adf4920b237e203f2d1fc03
Kjell Randa <kjell.m.randa@gmail.com>
2022-12-29 22:25:52 +0100

    Refactoring and more pylint cleanup

--- Gulen/max-power-usage.py
+++ Gulen/max-power-usage.py
@@ -30,51 +30,59 @@ def influx_json(val, timev, typev):
     ]
     return json_body
 
-logging.basicConfig(filename='maxusage.log', filemode='a', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info('Processing starting ...')
+def main():
+    """
+    Find 3 hours each month with highest energy usage
+    Store values in Influxdb
+    """
+    logging.basicConfig(filename='maxusage.log', filemode='a', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('Processing starting ...')
 
-arr = []
-pusage = []
-ustime = []
-maxuse = ["MAX1", "MAX2", "MAX3"]
+    arr = []
+    pusage = []
+    ustime = []
+    maxuse = ["MAX1", "MAX2", "MAX3"]
 
-year = datetime.now().year
-month = datetime.now().month
-SDATE = str(year) + "-" + str(month) + "-01T00:00:01Z"
+    year = datetime.now().year
+    month = datetime.now().month
+    SDATE = str(year) + "-" + str(month) + "-01T00:00:01Z"
 
-client = InfluxDBClient(host='localhost', port='8086', database='hansensor')
+    client = InfluxDBClient(host='localhost', port='8086', database='hansensor')
 
-SQL = "SELECT INTEGRAL(\"mean\")/3600 FROM ( SELECT MEAN(\"val\") AS mean FROM \"mqtt_consumer\" " + \
-      "WHERE \"topic\" = \'pt:j1/mt:evt/rt:dev/rn:zigbee/ad:1/sv:meter_elec/ad:1_1\' AND \"unit\" = \'W\' AND time <= now() and time >= \'" + \
-       SDATE + "\' GROUP BY time(5s) fill(previous) ) GROUP BY time(1h)"
-result = client.query(SQL)
+    SQL = "SELECT INTEGRAL(\"mean\")/3600 FROM ( SELECT MEAN(\"val\") AS mean FROM \"mqtt_consumer\" " + \
+          "WHERE \"topic\" = \'pt:j1/mt:evt/rt:dev/rn:zigbee/ad:1/sv:meter_elec/ad:1_1\' AND \"unit\" = \'W\' AND time <= now() and time >= \'" + \
+           SDATE + "\' GROUP BY time(5s) fill(previous) ) GROUP BY time(1h)"
+    result = client.query(SQL)
 
-NUM = 0
-points = result.get_points()
-for point in points:
-    arr.append([point['integral'],point['time']])
-    NUM = NUM + 1
+    num = 0
+    points = result.get_points()
+    for point in points:
+        arr.append([point['integral'],point['time']])
+        num = num + 1
 
-if NUM <= 3:
-    logging.warning("Not enough data to contine. Only " + str(n) + " found")
-    client.close()
-    exit(1)
+    if num <= 3:
+        logging.warning("Not enough data to contine. Only " + str(num) + " found")
+        client.close()
+        exit(1)
 
-arr.sort(reverse=True)
+    arr.sort(reverse=True)
 
-sumu = 0.0
-for i in range(3):
-    x, s = arr[i]
-    pusage.append(x/1000)
-    ustime.append(datetime_from_utc_to_local(s))
-    sumu = sumu + pusage[i]
-    jbody = influx_json(pusage[i], ustime[i], maxuse[i])
+    sumu = 0.0
+    for i in range(3):
+        x, s = arr[i]
+        pusage.append(x/1000)
+        ustime.append(datetime_from_utc_to_local(s))
+        sumu = sumu + pusage[i]
+        jbody = influx_json(pusage[i], ustime[i], maxuse[i])
+        client.write_points(jbody)
+
+    avg = sumu/3
+    jbody = influx_json(avg, ustime[0], "MAXAVG")
     client.write_points(jbody)
 
-avg = sumu/3
-jbody = influx_json(avg, ustime[0], "MAXAVG")
-client.write_points(jbody)
+    client.close()
 
-client.close()
+    logging.info('Processing finished ...')
 
-logging.info('Processing finished ...')
+if __name__ == "__main__":
+   main()
