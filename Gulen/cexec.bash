24746a30b1ea24c62778206d248088eb144c2b41
Kjell Randa <kjell.m.randa@gmail.com>
2023-01-02 00:13:08 +0100

    Shell lint fixex

--- Gulen/cexec.bash
+++ Gulen/cexec.bash
@@ -13,12 +13,12 @@ usage()
 #
 [ $# -ne 1 -a $# -ne 2 ] && usage
 #
-nc=`docker ps | grep -c $1`
-if [ $nc -eq 1 ]
+nc=$(docker ps | grep -c "$1")
+if [ "$nc" -eq 1 ]
 then
-   id=`docker ps | grep $1 | awk '{print $1}'`
+	id=$(docker ps | grep "$1" | awk '{print $1}')
    [ $# -eq 2 ] && CMD="$2"
-   docker exec --tty -it $id $CMD
+   docker exec --tty -it "$id" "$CMD"
 else
    $ECHO ""
    $ECHO "Containername not unique or not found"
