8d459018f9be9123a190c6a91c7ce677653389b1
Kjell Randa <kjell.m.randa@gmail.com>
2023-01-02 20:18:26 +0100

    Shell lint fixes

--- get-xfs-ver.sh
+++ get-xfs-ver.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 #
 printf "%-20s  %-9s %-8s\n" "Mountpoint" "Dev" "Ver"
-printf "%35s\n" | tr " " "="
+printf "%0.s=" {1..35}
+printf "\n"
 #
-for i in `grep xfs /etc/fstab | grep -v "^#" | awk '{print $1}'| cut -d= -f2`
+for i in $(grep xfs /etc/fstab | grep -v "^#" | awk '{print $1}'| cut -d= -f2)
 do
-	mnt=`grep $i /etc/fstab | grep -v "^#" | awk '{print $2}'`
+	mnt=$(grep $i /etc/fstab | grep -v "^#" | awk '{print $2}')
 	if [ -e /dev/disk/by-uuid/$i ]
 	then
-		dev=`ls -la /dev/disk/by-uuid/$i | awk '{print $11}' | sed 's/..\/..\///'`
+		dev=$(ls -la /dev/disk/by-uuid/$i | awk '{print $11}' | sed 's/..\/..\///')
 	else
-		dev=`ls -la $i | awk '{print $11}' | sed 's/..\///'`
+		dev=$(ls -la $i | awk '{print $11}' | sed 's/..\///')
 	fi
-	ver=`grep XFS /var/log/messages | grep Mounting | grep \($dev\) | cut -d"(" -f2 | sort -u | awk '{print $3}'`
+	ver=$(grep XFS /var/log/messages | grep Mounting | grep \($dev\) | cut -d"(" -f2 | sort -u | awk '{print $3}')
 	printf "%-20s  %-9s %-8s\n" $mnt $dev $ver
 done 
