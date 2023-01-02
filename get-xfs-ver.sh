8e4d913b50bb6fe2ab887ea6f4730476ccfa648e
Kjell Randa <kjell.m.randa@gmail.com>
2023-01-02 20:30:57 +0100

    Shell lint fixes

--- get-xfs-ver.sh
+++ get-xfs-ver.sh
@@ -6,13 +6,13 @@ printf "\n"
 #
 for i in $(grep xfs /etc/fstab | grep -v "^#" | awk '{print $1}'| cut -d= -f2)
 do
-	mnt=$(grep $i /etc/fstab | grep -v "^#" | awk '{print $2}')
-	if [ -e /dev/disk/by-uuid/$i ]
+	mnt=$(grep "$i" /etc/fstab | grep -v "^#" | awk '{print $2}')
+	if [ -e /dev/disk/by-uuid/"$i" ]
 	then
-		dev=$(ls -la /dev/disk/by-uuid/$i | awk '{print $11}' | sed 's/..\/..\///')
+		dev=$(ls -la /dev/disk/by-uuid/"$i" | awk '{print $11}' | sed 's/..\/..\///')
 	else
-		dev=$(ls -la $i | awk '{print $11}' | sed 's/..\///')
+		dev=$(ls -la "$i" | awk '{print $11}' | sed 's/..\///')
 	fi
 	ver=$(grep XFS /var/log/messages | grep Mounting | grep \($dev\) | cut -d"(" -f2 | sort -u | awk '{print $3}')
-	printf "%-20s  %-9s %-8s\n" $mnt $dev $ver
+	printf "%-20s  %-9s %-8s\n" "$mnt" "$dev" "$ver"
 done 
