ea776e2a6c3d9d95ef00c5dbc9f9d4d88f9b1646
Kjell Randa <kjell.m.randa@gmail.com>
2024-01-04 14:42:40 +0100

    Add regulation of setpoints

--- Gulen/NodeRed/evaluatePowerUsage.js
+++ Gulen/NodeRed/evaluatePowerUsage.js
@@ -2,6 +2,13 @@ let power = msg.payload;
 let n = power.length - 1;
 const date = new Date();
 let current = date.getMinutes();
+const maxLimit = 4.8;
+
+var usedP = context.get('usedP');
+if (typeof usedP == "undefined") {
+    usedP = "low";
+    context.set('usedP', usedP);
+}
 
 let estimate = 0;
 if (current > 0) {
@@ -9,7 +16,24 @@ if (current > 0) {
 }
 
 msg = {};
-msg.payload = power[n].integral;
 node.log("");
-node.log("Used so far " + msg.payload.toFixed(2) + " KWh. Estimated houarly usage " + estimate.toFixed(2) + " KWh");
-return msg;
+node.log("Used so far " + power[n].integral.toFixed(2) + " KWh. Estimated houarly usage " + estimate.toFixed(2) + " KWh");
+
+if (current > 20 && estimate > maxLimit) {
+    msg.payload = "high";
+    node.log("High estimated power usage. Lowering setpoints");
+    usedP = "high";
+    context.set('usedP', usedP);
+    return msg;
+} else {
+    usedP = context.get('usedP');
+    if (usedP == "high") {
+        usedP = "low";
+        context.set('usedP', usedP);
+        msg.payload = "low";
+        node.log("Power usage reset to low");
+        return msg;
+    }
+    return null;
+}
+return null;
