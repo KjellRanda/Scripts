883cdf6f7661b36ef225136449c0f080ebe54675
Kjell Randa <kjell.m.randa@gmail.com>
2024-01-31 21:36:51 +0100

    Revert "Added from home"
    This reverts commit 2b3a098f395bbf33a21ee19b188b959f72a88ae4.

--- backup2google.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#
-BCK_HOME="/dl/scratch/backup"
-#
-rclone -v sync --log-file $BCK_HOME/backup.log --include nazgul-*     $BCK_HOME GoogleDrive:Linux-backup/Nazgul
-rclone -v sync --log-file $BCK_HOME/backup.log --include eagle-*      $BCK_HOME GoogleDrive:Linux-backup/Eagle
-rclone -v sync --log-file $BCK_HOME/backup.log --include hobbit-*     $BCK_HOME GoogleDrive:Linux-backup/Hobbit
-rclone -v sync --log-file $BCK_HOME/backup.log --include gatekeeper-* $BCK_HOME GoogleDrive:Linux-backup/Gatekeeper
--- fix-backup-prot.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#
-BCK_HOME="/dl/scratch/backup"
-#
-chown -R anonuid:backup $BCK_HOME
-find $BCK_HOME -type d -exec chmod 2750 {} \;
-find $BCK_HOME -type f -exec chmod 640 {} \;
-#
