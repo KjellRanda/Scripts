d0705d57a2e14073faa631c0b432d1619ee342c2
Kjell Randa <kjell.m.randa@gmail.com>
2024-08-07 12:08:32 +0200

    Add monitoring of Tuya based dehumidifier

---
+++ Gulen/tuya-dehumidifier.py
@@ -0,0 +1,36 @@
+import time
+import json
+import tinytuya
+from influxdb import InfluxDBClient
+
+DEVICEFILE = "devices.json"
+
+f = open(DEVICEFILE)
+data = json.load(f)
+f.close()
+
+id = data[0]['id']
+key = data[0]['key']
+ip = data[0]['ip']
+ver = float(data[0]['version'])
+
+map = data[0]['mapping']
+
+num = [2, 6, 18, 19]
+
+client = InfluxDBClient(host='localhost', port='8086', database='hansensor')
+
+while True:
+    d = tinytuya.Device(id, ip, key, version=ver)
+    devdata = d.status()
+    message = '[{' +  '"measurement": "tuya", "tags": {"name": "avfukter", "ipaddr": "' + ip + '"}, "fields": {'
+    for item in devdata['dps']:
+        if int(item) in num:
+            message += '"' + str(map[item]['code']) + '": ' + str(devdata['dps'][item]) + ', '
+        else:
+            message += '"' + str(map[item]['code']) + '": "' + str(devdata['dps'][item]) + '", '
+    message = message[:-2]
+    message += '}}]'
+    print(json.dumps(json.loads(message), indent=4))
+    client.write_points(json.loads(message))
+    time.sleep(600)
