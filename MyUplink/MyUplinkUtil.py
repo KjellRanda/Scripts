e1829b32bde3ef20838ade0ec0bb3a52222c2f6b
Kjell Randa <kjell.m.randa@gmail.com>
2024-11-22 14:00:39 +0100

    Added logger class

--- MyUplink/MyUplinkUtil.py
+++ MyUplink/MyUplinkUtil.py
@@ -1,5 +1,7 @@
 import configparser
 import os
+import logging
+import logging.handlers
 
 class MyUptimeConfig:
     def __init__(self, filename) -> None:
@@ -14,3 +16,24 @@ class MyUptimeConfig:
             return self.config[key][val]
         except KeyError:
             return ""
+
+class myLogger:
+    def __init__(self, logfile, version) -> None:
+        logger = logging.getLogger('__name__')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s ' + logfile + ' ver:' + str(version) + ' %(levelname)s %(message)s')
+        handler = logging.handlers.RotatingFileHandler(logfile + ".log", mode='a', maxBytes=8388608, backupCount=16)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        self.logger = logger
+        self.logfile = logfile
+
+    def getLogger(self):
+        self.logger.info("Starting ...")
+        return self.logger
+    
+    def setLoggerLevel(self, level: str):
+        goodLevels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+        level = level.upper()
+        if level in goodLevels:
+            self.logger.setLevel(level)
